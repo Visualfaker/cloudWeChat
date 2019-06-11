a06be36f67951346e7e063fdcb65e7be10b9c7a8
dogbo <804276027@qq.com>
2019-06-11 21:38:16 +0800

    fix(index):swiper新增
    明天：静态模拟一账通

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,14 +8,20 @@ Page({
     msg: '代码测试',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    indicatorDots: true,
+    circular: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 500,
+    indicatorActiveColor: "red"
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
